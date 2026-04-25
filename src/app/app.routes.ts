2210408f37635a7325f49f922c9eb49c5e96f740
Juan Romero <nanone99@gmail.com>
2026-04-25 14:16:19 +0000

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'movie-details',
+    loadComponent: () => import('./movie-details/movie-details.page').then( m => m.MovieDetailsPage)
+  },
+  {
+    path: 'details',
+    loadComponent: () => import('./details/details.page').then( m => m.DetailsPage)
+  },
+  {
+    path: 'favourites',
+    loadComponent: () => import('./favourites/favourites.page').then( m => m.FavouritesPage)
+  },
+];

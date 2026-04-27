a7cedb3753e0a19137b0c06d6c4975618c2dec5e
Juan Romero <nanone99@gmail.com>
2026-04-27 07:39:12 +0000

    home and navigations buttons added

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { FavouritesService } from '../services/favourites.service';
 import { Router } from '@angular/router';
-import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonImg, IonHeader, IonToolbar } from "@ionic/angular/standalone";
+import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonImg, IonHeader, IonToolbar, IonButtons, IonIcon, IonTitle } from "@ionic/angular/standalone";
 import { NgFor } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
-  imports: [IonToolbar, IonHeader, IonContent,IonCard,IonCardHeader,IonCardTitle,IonButton,IonImg,NgFor]
+  imports: [IonTitle, IonIcon, IonButtons, IonToolbar, IonHeader, IonContent,IonCard,IonCardHeader,IonCardTitle,IonButton,IonImg,NgFor]
 })
 export class FavouritesPage {
 
   favouriteMovies: any[] = [];
 
-  constructor(private favs: FavouritesService, private router: Router) {}
+  constructor(private favs: FavouritesService, private router: Router) {
+    addIcons({
+      home
+    });
+
+  }
 
   async ionViewWillEnter() {
     this.favouriteMovies = await this.favs.get();
@@ -23,5 +30,9 @@ export class FavouritesPage {
   openDetails(id: number) {
     this.router.navigate(['/movie-details', id]);
   }
+
+  goHome() {
+    this.router.navigate(['/home']);
+  } 
 }
 
--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -3,16 +3,18 @@ import { ActivatedRoute } from '@angular/router';
 import { MyHttp } from '../services/my-http';
 import { HttpOptions } from '@capacitor/core';
 import { NgFor, NgIf } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,IonButton } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,IonButton, IonButtons, IonIcon } from "@ionic/angular/standalone";
 import { Router } from '@angular/router';
 import { FavouritesService } from '../services/favourites.service';
+import { addIcons } from 'ionicons';
+import { heart, heartOutline, heartSharp, home } from 'ionicons/icons';
 
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, NgFor, NgIf, IonButton],
+  imports: [IonIcon, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, NgFor, NgIf, IonButton],
 })
 
 export class MovieDetailsPage {
@@ -24,7 +26,17 @@ export class MovieDetailsPage {
 
   creditsBaseUrl: string = "https://api.themoviedb.org/3/movie/";
 
-    constructor(private route: ActivatedRoute, private mhs: MyHttp, private router: Router,private favs: FavouritesService) {}
+    constructor(private route: ActivatedRoute, private mhs: MyHttp, private router: Router,private favs: FavouritesService) {
+    
+      //Source: https://stackoverflow.com/questions/77726607/ionicons-are-not-displayed-in-ionic-7-1-1-angular-17-0-8
+    addIcons({
+      heart,
+      heartOutline,
+      heartSharp,
+      home
+    });
+
+    }
 
   ngOnInit() {
     this.movieId = Number(this.route.snapshot.paramMap.get('id'));
@@ -53,6 +65,17 @@ export class MovieDetailsPage {
   async addToFavourites() {
     await this.favs.add(this.movieDetails);
   }
+
+  //Source: https://stackoverflow.com/questions/40245847/how-to-go-to-another-page-with-a-button-click-with-ionic
+  goToFavourites() {
+    this.router.navigate(['/favourites']);
+  }  
+
+    //Source: https://stackoverflow.com/questions/40245847/how-to-go-to-another-page-with-a-button-click-with-ionic
+    goHome() {
+      this.router.navigate(['/home']);
+    }  
+      
   
 }
 
